bd7e1288d2bbf934b01b7fcc4d7955157f426bb3
holawaleh <holawaleh@gmail.com>
2026-04-21 21:28:53 +0100

    setting up backend for render integeration

--- backdoor/camalert/settings.py
+++ backdoor/camalert/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 # Load environment variables from .env file
 load_dotenv()
@@ -96,10 +97,9 @@ WSGI_APPLICATION = "camalert.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        os.getenv("DATABASE_URL"), conn_max_age=600, ssl_require=True
+    )
 }
 
 # Password validation
